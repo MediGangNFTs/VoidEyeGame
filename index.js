7dcd9095bcdb5a46f19262f1cdb0949557730f76
MediGangNFTs <149858228+MediGangNFTs@users.noreply.github.com>
2025-06-09 07:34:38 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+
+import dynamic from 'next/dynamic';
+
+const GameCanvas = dynamic(() => import('../components/GameCanvas'), { ssr: false });
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', padding: '20px', background: 'black', color: 'white' }}>
+      <h1>Void Tower Defense</h1>
+      <GameCanvas />
+    </div>
+  );
+}
